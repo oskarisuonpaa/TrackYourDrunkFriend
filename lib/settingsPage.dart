439149b67da65e67c1e6e4974f727ee2454abdb9
Topi Ilmoni <topi.ilmoni@gmail.com>
2021-04-29 13:41:01 +0300

    Added settings page

--- lib/settingsPage.dart
+++ lib/settingsPage.dart
@@ -3,7 +3,104 @@ import 'package:flutter/material.dart';
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(15.0),
+          child: Text(
+            "General settings",
+            style: TextStyle(fontSize: 18)
+        )),
+        Container(
+          constraints: BoxConstraints(maxWidth: 500, minHeight: 300),
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(3.0),
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Dark mode"),
+                  Switch(
+                    value: false,
+                  ),
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Give data for commercial use "),
+                  Switch(
+                    value: false,
+                  ),
+                ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Allow mobile data usage: "),
+                  Switch(
+                    value: false,
+                  ),
+                ]),
+            ],
+          ),
+          ),
+        Container(
+            margin: const EdgeInsets.all(15.0),
+            child: Text(
+                "About",
+                style: TextStyle(fontSize: 18)
+            )),
+        Container(
+          constraints: BoxConstraints(maxWidth: 500, minHeight: 300),
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(3.0),
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Column(
+            children: <Widget>[
+              Row(
+                  children: [
+                    Icon(
+                      Icons.settings,
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                    Text("Terms and Privacy Policy"),
+                  ]),
+              Row(
+                  children: [
+                    Icon(
+                      Icons.insert_drive_file,
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                    Text("Acknowledgments"),
+                  ]),
+              Row(
+                  children: [
+                    Icon(
+                      Icons.help_center_outlined,
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                    Text("Help"),
+                  ]),
+              Row(
+                  children: [
+                    Icon(
+                      Icons.info_outline,
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                    Text("App info"),
+                  ]),
+            ],
+          ),
+        ),
+      ]
+    )
+    );
   }
 }
 
